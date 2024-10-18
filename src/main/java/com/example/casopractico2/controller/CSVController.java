78051a902763d6f31f1868f53a9fd372e6714f1c
Tapiiaa <ptapilob@myuax.com>
2024-10-18 15:48:53 +0200

    Se asignan las tareas a los hilos

--- src/main/java/com/example/casopractico2/controller/CSVController.java
+++ src/main/java/com/example/casopractico2/controller/CSVController.java
@@ -1,22 +1,22 @@
 package com.example.casopractico2.controller;
 
-import com.example.casopractico2.model.BiologicalSample;
-import com.example.casopractico2.service.CSVService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.casopractico2.service.DataProcessingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class CSVController {
 
-    @Autowired
-    private CSVService csvService;
+    private final DataProcessingService dataProcessingService;
 
-    @GetMapping("/import-async")
-    public CompletableFuture<List<BiologicalSample>> importDataAsync() {
-        return csvService.importCSVAsync("src/main/resources/biological_data.csv");
+    public CSVController(DataProcessingService dataProcessingService) {
+        this.dataProcessingService = dataProcessingService;
+    }
+
+    // Este es el mapeo que debes verificar
+    @GetMapping("/process-biological-data")
+    public String processBiologicalData() {
+        dataProcessingService.processBiologicalDataWithSemaphore();
+        return "Data processing started";
     }
 }
