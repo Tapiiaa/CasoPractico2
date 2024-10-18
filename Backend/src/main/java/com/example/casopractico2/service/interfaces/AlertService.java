83612c3d02604927878d20d369efa7ce0aab9a84
Patrik <psirb@myuax.com>
2024-10-19 00:40:08 +0200

    Arreglo de errores. Hay que agregar AlertListener mas tarde

--- Backend/src/main/java/com/example/casopractico2/service/interfaces/AlertService.java
+++ Backend/src/main/java/com/example/casopractico2/service/interfaces/AlertService.java
@@ -2,6 +2,6 @@ package com.example.casopractico2.service.interfaces;
 
 public interface AlertService {
     void sendAlert(String message);
-    void registerAlertListener(AlertListener listener);
-    void removeAlertListener(AlertListener listener);
+    void registerAlertListener();   // AlertListener listener -> Agregar luego
+    void removeAlertListener();
 }
