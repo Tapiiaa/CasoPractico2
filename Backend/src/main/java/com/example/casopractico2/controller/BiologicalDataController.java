74611e0359d7489ba2938a8ab562519466475a1a
Patrik <psirb@myuax.com>
2024-10-20 00:50:42 +0200

    Arreglo de mapping :)

--- Backend/src/main/java/com/example/casopractico2/controller/BiologicalDataController.java
+++ Backend/src/main/java/com/example/casopractico2/controller/BiologicalDataController.java
@@ -2,6 +2,7 @@ package com.example.casopractico2.controller;
 
 import com.example.casopractico2.model.BiologicalAttributes;
 import com.example.casopractico2.service.impl.BiologicalDataProcessor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,11 @@ import java.util.List;
 @RestController
 public class BiologicalDataController {
     private final BiologicalDataProcessor biologicalDataProcessor;
-
     public BiologicalDataController(BiologicalDataProcessor biologicalDataProcessor) {
         this.biologicalDataProcessor = biologicalDataProcessor;
     }
 
-    @PostMapping("/api/biological-data")
+    @GetMapping("/api/biological-data")
     public List<BiologicalAttributes> getBiologicalData(){
         return biologicalDataProcessor.getProcessedData();
     }
