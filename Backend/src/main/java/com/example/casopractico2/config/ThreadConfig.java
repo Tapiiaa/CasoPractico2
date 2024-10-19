c7c16c6e9dcfcddbe8f8c0510dbc68a0f560fab9
itsedupablo <pedugar@myuax.com>
2024-10-19 12:37:00 +0200

    Error con la función threadCount solucionado aparentemente

--- Backend/src/main/java/com/example/casopractico2/config/ThreadConfig.java
+++ Backend/src/main/java/com/example/casopractico2/config/ThreadConfig.java
@@ -1,8 +1,8 @@
 package com.example.casopractico2.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,8 +10,16 @@ import java.util.concurrent.Executors;
 @Configuration
 public class ThreadConfig {
 
-    @Bean(name = "customExecutorService")  // Cambiamos el nombre para evitar conflicto
+    @Bean(name = "customExecutorService")
+    @Primary
+
     public ExecutorService taskExecutor() {
-        return Executors.newFixedThreadPool(5);  // Pool de hilos fijo
+        return Executors.newFixedThreadPool(8);  // Pool de hilos fijo
+    }
+
+    // Definir un bean de tipo int para inyectar en DataSynchronizationManager
+    @Bean
+    public int threadCount() {
+        return 8;  // Número de hilos que quieres usar para sincronización
     }
 }
