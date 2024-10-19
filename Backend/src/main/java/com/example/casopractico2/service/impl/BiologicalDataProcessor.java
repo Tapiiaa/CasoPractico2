55e1971bc0fdea4bd9f94151de25a57663368f8f
Patrik <psirb@myuax.com>
2024-10-20 00:11:24 +0200

    Rest controller de BiologicalDataController

--- Backend/src/main/java/com/example/casopractico2/service/impl/BiologicalDataProcessor.java
+++ Backend/src/main/java/com/example/casopractico2/service/impl/BiologicalDataProcessor.java
@@ -1,8 +1,14 @@
 package com.example.casopractico2.service.impl;
 
 import com.example.casopractico2.model.BiologicalAttributes;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class BiologicalDataProcessor {
+    private List<BiologicalAttributes> processedData = new ArrayList<>();
 
     public void processBiologicalAtributtes(BiologicalAttributes biologicalAttributes) {
         System.out.println("Procesando atributos biológicos: " + biologicalAttributes);
@@ -11,4 +17,9 @@ public class BiologicalDataProcessor {
         System.out.println("Color de la flor: " + biologicalAttributes.getFlowerColor());
         System.out.println("Habito de crecimiento: " + biologicalAttributes.getGrowthHabit());
     }
+
+    public List<BiologicalAttributes> getProcessedData(){
+        return processedData;
+    }
+
 }
