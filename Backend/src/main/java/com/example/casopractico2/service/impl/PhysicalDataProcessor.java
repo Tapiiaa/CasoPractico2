5d353ed5d8657d98005560109b37697f393a906f
Patrik <psirb@myuax.com>
2024-10-19 23:34:41 +0200

    Procesador de los atributos fisicos de las plantas.

--- Backend/src/main/java/com/example/casopractico2/service/impl/PhysicalDataProcessor.java
+++ Backend/src/main/java/com/example/casopractico2/service/impl/PhysicalDataProcessor.java
@@ -1,4 +1,11 @@
 package com.example.casopractico2.service.impl;
 
+import com.example.casopractico2.model.PhysicalAttributes;
+
 public class PhysicalDataProcessor {
+    public void processPhysicalAtributtes(PhysicalAttributes physicalAttributes){
+        System.out.println("Procesando atributos físicos: ");
+        System.out.println("Altura: " + physicalAttributes.getHeight() + "metros");
+        System.out.println("Zona geográfica: " + physicalAttributes.getLocationZone());
+    }
 }
